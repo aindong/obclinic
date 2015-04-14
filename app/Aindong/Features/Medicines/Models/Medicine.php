1b0961bb15571f497c9e2f81500da9784aa5612a
Alleo Indong <freedom_war550@live.com.ph>
2015-04-15 00:34:04 +0800

    Model configuration

--- app/Aindong/Features/Medicines/Models/Medicine.php
+++ app/Aindong/Features/Medicines/Models/Medicine.php
@@ -2,5 +2,8 @@
 namespace Aindong\Features\Medicines\Models;
 
 class Medicine extends \Eloquent {
+    protected $table = 'medicines';
+    protected $guarded = ['id'];
 
+    protected $rules = ['name' => 'required'];
 }
