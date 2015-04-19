844d96b2f67048a711982ae1197a6db25c2756e9
Alleo Indong <freedom_war550@live.com.ph>
2015-04-19 14:49:35 +0800

    created store method on appointment

--- app/Aindong/Features/Appointments/routes.php
+++ app/Aindong/Features/Appointments/routes.php
@@ -8,5 +8,6 @@ Route::group(['namespace' => 'Aindong\Features\Appointments\Controllers\Api',
     'prefix' => '/api/v1/'], function() {
 
     Route::get('appointments', 'AppointmentsController@index');
+    Route::post('appointments', 'AppointmentsController@store');
 
 });
