09d6cd96851adc1c6ed2cd7a5dffa5a4402f4059
Alleo Indong <freedom_war550@live.com.ph>
2015-05-15 23:07:50 +0800

    updated database settings

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => 'localhost',
 			'database'  => 'obclinic',
 			'username'  => 'root',
-			'password'  => 'molecules',
+			'password'  => '',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
